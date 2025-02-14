386cb0d49b34dc04b9d7ad1e84d494f7a470cc17
Abraar Sadek <abraarsadek@gmail.com>
2025-02-13 21:35:29 -0500

    Player Dash - 02/13/2025
    Added:
    
         - Basic Character Dashing
         - Basic First Person Camera
    
    Updated:
    
         - Basic Character Movement
         - Basic Character Jump
    
    Removed:
    
         - Third Person Following Free Look Camera

--- Ballin/Assets/_Project/Scripts/playerJumpController.cs
+++ Ballin/Assets/_Project/Scripts/playerJumpController.cs
@@ -5,54 +5,69 @@
  * Date Created: 02/10/2025
  * Last Modified: 02/10/2025
  * 
- * Script Purpose: To control the basic upwards jump of the player...
+ * Script Purpose: To control the basic upwards jump ability of the player...
  * 
  */
 
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 //playerJumpController Class
 public class playerJumpController : MonoBehaviour {
 
+    //Referance To Other Scripts
+    playerMovementController playerMovement; //Referance To The playerMovementController Script
+
     //Public Variables
-    public float jumpForce = 8f;  // Jump force
-    public float raycastDistance = 1.1f; // Distance to check for ground
-    public LayerMask groundLayer; // Define what is considered "ground"
+    [Header("Player Jump: ")]
+    public float jumpForce; //Float variable that will hold the players jump force
+    public float jumpCooldown; //Float variable that will hold the players jump cooldown
+    public float airMultiplier; //Float variable that will hold the players jump air multiplier
+    
+    //Private Variables
+    private bool readyToJump = true; //Bool variable that will check if the player is ready to jump
 
     //Private Variables
-    private Rigidbody rb;
+    private Rigidbody rb; //Reference to Rigidbody Attached To Player
+
+    //playerJumpController Class Getter & Setter
+    public bool GetReadyToJump { get { return readyToJump; } }
+    public void SetReadyToJump(bool value) { readyToJump = value; }
+    public float GetJumpCoooldown { get { return jumpCooldown; } }
+    public float GetAirMultiplier { get { return airMultiplier; } }
+    //End of playerJumpController Class Getter & Setter
 
     //Start Method
     void Start() {
         rb = GetComponent<Rigidbody>();
+        playerMovement = GetComponent<playerMovementController>();
     } //End of Start Method
 
-    //Update Method
-    void Update() {
-        //If-Statement - To Check If The Player Is On The Ground, And If So, Call The Jump Method
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
-            Jump();
-        } //End of If-Statement
-    } //End of Update Method
-
     //Jump Method
-    void Jump() {
-        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z); // Reset Y velocity to prevent stacking forces
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    private void Jump() {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z); //Reset players 'y' velocity to zero
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); //Add force to the player to make them jump
     } //End of Jump Method
 
-    //IsGrounded Method
-    bool IsGrounded() {
-        //Casts a ray downward to check if the ball is on the ground
-        return Physics.Raycast(transform.position, Vector3.down, raycastDistance, groundLayer);
-    } //End of IsGrounded Method
+    //PreformJump Method
+    public void PreformJump() { Jump(); } //End of PreformJump Method
 
-    //OnDrawGizmos Method
-    void OnDrawGizmos() {
-        // Visualizes the raycast in the scene view
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * raycastDistance);
-    } //End of OnDrawGizmos Method
+    //ResetJump Method
+    private void ResetJump() { 
+        readyToJump = true; //Set the 'readyToJump' variable to true
+    } //End of ResetJump Method
+
+    //CallResetJump Method
+    public void CallResetJump() {
+        //ResetJump(); 
+        StartCoroutine(ResetJumpAfterDelay());
+    } //End of CallResetJump Method
+
+    //ResetJumpAfterDelay IEnumerator
+    private IEnumerator ResetJumpAfterDelay() {
+        yield return new WaitForSeconds(jumpCooldown);
+        ResetJump();
+    } //End of ResetJumpAfterDelay IEnumerator
 
 } //End of playerJumpController Class
-
